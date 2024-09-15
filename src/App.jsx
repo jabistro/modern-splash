ecd79220e1838c740bc55cfad77d001f7d5a5fd7
John Allan Hinds <jabistro@gmail.com>
2024-09-14 23:12:55 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 } from "./components";
 
 const App = () => {
-  alert("Links and buttons are nonfunctional. This is just a demo splashhhh page.");
+  alert("Links and buttons are nonfunctional. This is just a demo splashhhhh page.");
 
   return (
     <div className="bg-primary w-full overflow-hidden">
